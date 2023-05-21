069c947496308ef6600155287b82bdc0acd2c3e5
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-05-21 10:35:52 -0500

    tailwind funcionando

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () =>  (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello,world </h1>
+    <h1 className='text-6xl bg-slate-600'>Hello, bitch </h1>
   </div>
 );
 
