8f4c5a6846d651bb06072dd000b11b09c3bb69a3
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-05-22 14:15:01 -0500

    Cards fixed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
 import Hero from './components/hero'
 import Services from './components/services'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      
       <Hero/>
-      
       <Services/>
     </div>
   )
--- Client/src/components/services.jsx
+++ Client/src/components/services.jsx
@@ -1,26 +1,45 @@
 import React from 'react'
 import './style.css'
+import Card from './card'
 
 const Services = () => {
   return (
     <div className='hero back2 align-items-center' >
-      <div className=" flex-column  text-center p-3 exp">
-        <div className="row ">
-          <div className="col">
-            1 of 2
+      <div className=" flex-column  text-center space ">
+        <div className="row exp align-items-center w-100">
+          <div className="col us">
+            <div className='mx-5 '>
+              Experimienta los beneficios de adquirir tu vehículo con <font color="Red"> nosotros </font> .
+            </div>
+            
           </div>
           <div className="col">
-            <div className='row'>
+            <div className='row '>
               <div className="col">
-                1 of 2
+                <Card/>
               </div>
               <div className="col">
-                2 of 2
+                <Card/>
               </div>
             </div>
             
           </div>
         </div>
+        
+        <div className="row exp align-items-center w-100">
+          <div className="col">
+            <Card/>
+          </div>
+          <div className="col">
+            <Card/>
+          </div>
+          <div className="col">
+            <Card/>
+          </div>
+          <div className="col">
+            <Card/>
+          </div>
+        </div>  
         
       </div>      
     </div>
