5856fb29b958fca82f35e94c09f79866fb4a75d2
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2023-05-22 09:58:18 -0500

    conexion con base de datos de elephant

--- lithium/lithium/settings.py
+++ lithium/lithium/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()   #Cargar variables de ambiente
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7c!lr1qx3kpd_xd92yi%yd09rf6^!cs3&x8@*p07^wd2k-3#-a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage', #Para almacenar las imagenes
+    'cloudinary',  #Para almacenar las imagenes
+    'rest_framework', #Provee funcionalidades para facilitar la construcción de APIs
+    'corsheaders',  #Para configurar que dominios pueden hacer peticiones a la app
+    'login', 
 ]
 
 MIDDLEWARE = [
@@ -74,10 +82,14 @@ WSGI_APPLICATION = 'lithium.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('POSTGRES_NAME'),
+       'USER': os.getenv('POSTGRES_USER'),
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+       'HOST': os.getenv('POSTGRES_HOST'),
+       'PORT':os.getenv('POSTGRES_PORT'),
+   }
 }
 
 
