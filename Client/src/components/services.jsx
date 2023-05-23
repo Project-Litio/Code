5d999ff88cee34482323b2af22681e1e61826a35
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-05-22 19:25:19 -0500

    services done

--- Client/src/components/services.jsx
+++ Client/src/components/services.jsx
@@ -37,44 +37,38 @@ const cards = [
 
 const Services = () => {
   return (
-    <div className='serv back2 align-items-center' >
-      <div className=" flex-column  text-center space  ">
-        <div className="  row exp align-items-center w-100">
-          <div className="col us">
-            <div className='mx-5 '>
-              Experimienta los beneficios de adquirir tu vehículo con <font color="Red"> nosotros </font> .
+    <div className='serv back2 align-items-center ' >        
+        
+        <div className="row exp  align-items-center w-100 px-5 ">
+          <div className="us col-12  ">
+            <div className='mx-5'>
+              Experimienta los beneficios de adquirir tu vehículo con <font color="Red"> nosotros</font>.
             </div>
             
           </div>
-          <div className="col">
-            <div className='row '>
-              <div className="col">
-                <Card title={cards[0].title} text={cards[0].test} />
-              </div>
-              <div className="col">
-              <Card title={cards[1].title} text={cards[1].test} />
-              </div>
-            </div>
-            
-          </div>
+          
         </div>
-        
-        <div className="row exp align-items-center w-100">
-          <div className="col">
-          <Card title={cards[2].title} text={cards[2].test} />
-          </div>
-          <div className="col">
-          <Card title={cards[3].title} text={cards[3].test} />
-          </div>
-          <div className="col">
-          <Card title={cards[4].title} text={cards[4].test} />
-          </div>
-          <div className="col">
-          <Card title={cards[5].title} text={cards[5].test} />
-          </div>
+
+        <div className="row justify-content-between   align-items-center w-100 px-5 py-5">
+          <div className="col-12 col-sm-6 col-md-4 py-2">
+            <Card title={cards[0].title} text={cards[0].test} />
+            </div>
+            <div className="col-12 col-sm-6 col-md-4  py-2">
+              <Card title={cards[1].title} text={cards[1].test} />
+            </div>
+            <div className="col-12 col-sm-6 col-md-4  py-2">
+              <Card title={cards[2].title} text={cards[2].test} />
+            </div>
+            <div className="col-12 col-sm-6 col-md-4  py-2">
+              <Card title={cards[3].title} text={cards[3].test} />
+            </div>
+            <div className="col-12 col-sm-6 col-md-4  py-2">
+              <Card title={cards[4].title} text={cards[4].test} />
+            </div>
+            <div className="col-12 col-sm-6 col-md-4  py-2">
+              <Card title={cards[5].title} text={cards[5].test} />
+            </div>
         </div>  
-        
-      </div>      
     </div>
   )
 }
