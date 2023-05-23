d33d7a9e2257b2bf0dc580638da80acb1686f281
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-05-23 10:33:29 -0500

    fixed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Hero from './components/hero'
 import Services from './components/services'
-import Card from './components/card'
+
 
 function App() {
   const [count, setCount] = useState(0)
