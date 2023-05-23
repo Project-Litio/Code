ec12ac102d688cfd42c4a76f0a2f044c56b9396e
Kaos0w0 <brayan.andres.sanchez@correounivalle.edu.co>
2023-05-22 20:17:37 -0500

    Página de Login construida

---
+++ Client/src/pages/login.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LoginForm from '../components/LoginForm.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <LoginForm />
+)
