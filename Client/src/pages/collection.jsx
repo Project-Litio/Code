d94b14fe98a32454ef222c20eca93332ce2e1582
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-05-23 22:24:50 -0500

    carousel done

--- Client/src/pages/collection.jsx
+++ Client/src/pages/collection.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Carousel from '../components/carousel'
 const Collection = () => {
   return (
     <div>
     <Navbar />
+    <Carousel></Carousel>
     <Footer/>
     </div>
 
