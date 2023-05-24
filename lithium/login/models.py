abbb9a8a9c628dac163a8328a4fc314d4635ee41
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2023-05-23 21:39:37 -0500

    add users model

--- lithium/login/models.py
+++ lithium/login/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Branch(models.Model):
+    address=models.CharField(max_length=20)
+    city=models.CharField(max_length=20)
+
+class Employee(models.Model):
+    name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    email=models.EmailField()
+    password=models.CharField(max_length=120)
+    address=models.CharField(max_length=30)
+    phone=models.CharField(max_length=12)
+    id_branch=models.ForeignKey(Branch,on_delete=models.SET_NULL,null=True)
+
+class Manager(models.Model):
+    id_employee=models.ForeignKey(Employee,on_delete=models.CASCADE)
+
+class Mecanic(models.Model):
+    id_employee=models.ForeignKey(Employee,on_delete=models.CASCADE)
+
+class Saleman(models.Model):
+    id_employee=models.ForeignKey(Employee,on_delete=models.CASCADE)
+
+class Customer(models.Model):
+    CUSTOMER_TYPES = [
+        ("N","Natural"),
+        ("J","Juridico")
+    ]
+    
+    id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30,null=True)
+    password=models.CharField(max_length=120)
+    address=models.CharField(max_length=30)
+    phone=models.CharField(max_length=12)
+    email=models.EmailField()
+    type=models.CharField(max_length=1,choices=CUSTOMER_TYPES)
